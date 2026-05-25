158a094880615bdabe66446f6c47ab9389eba1fe
Slyvred <remihage2003@gmail.com>
2026-05-25 13:06:39 +0200

    chore(camo_page): remove deadcode

--- src/ui/camo_page.rs
+++ src/ui/camo_page.rs
@@ -7,8 +7,6 @@ use dioxus::prelude::*;
 pub fn CamoPage(page: Signal<Page>) -> Element {
     rsx! {
         div {
-            // style: "columns: 3 280px; gap: 1.5rem; padding: 25px; width: 100%; max-width: 98vw; margin: 3rem -1.5rem;",
-            // class: "columns-1 sm:columns-2 lg:columns-3 gap-6 p-6 w-full max-w-[98vw] my-12 -mx-6",
             class: "columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-6 p-6 w-full max-w-[98vw] my-12 -mx-6",
             id: "camo-page",
 
@@ -19,7 +17,6 @@ pub fn CamoPage(page: Signal<Page>) -> Element {
             }
 
             Button {
-                // style: "z-index: 1; position: fixed; bottom: 1.25rem; right: 1.25rem;",
                 class: "z-10 fixed bottom-5 right-5",
                 variant: ButtonVariant::Outline,
                 onclick: move |_| {
